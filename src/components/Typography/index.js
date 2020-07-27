662943f32cd58b974acdede3a87a9036fb91f763
Arthur Araujo <arthuraraujo@pop-os.localdomain>
2020-07-27 19:50:23 -0300

    move typography to one unique file

--- src/components/Typography/index.js
+++ src/components/Typography/index.js
@@ -1,6 +1,46 @@
-import Display1 from "./Display1";
-import Display2 from "./Display2";
-import H1 from "./H1";
+import styled from "styled-components";
+
+const Display1 = styled.h3`
+  font-family: Inter, sans-serif;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 64px;
+  line-height: 77px;
+
+  color: ${(props) => props.theme.colors[props.color]};
+`;
+
+Display1.defaultProps = {
+  color: "dark500",
+};
+
+const Display2 = styled.h4`
+  font-family: Inter, sans-serif;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 56px;
+  line-height: 68px;
+
+  color: ${(props) => props.theme.colors[props.color]};
+`;
+
+Display2.defaultProps = {
+  color: "dark500",
+};
+
+const H1 = styled.h1`
+  font-family: Inter, sans-serif;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 40px;
+  line-height: 54px;
+
+  color: ${(props) => props.theme.colors[props.color]};
+`;
+
+H1.defaultProps = {
+  color: "dark500",
+};
 
 const Typography = {
   Display1,
