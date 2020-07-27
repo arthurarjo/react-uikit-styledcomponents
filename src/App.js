1876a19ce3dbed19b9ccefb0e955a8b07583a943
Arthur Araujo <arthuraraujo@pop-os.localdomain>
2020-07-27 16:28:01 -0300

    add styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 import Router from "./router";
+import theme from "./theme";
+import { ThemeProvider } from "styled-components";
+
+import { Reset } from "styled-reset";
 
 function App() {
   return (
-    <div className="App">
-      <Router />
+    <div className="app">
+      <Reset />
+      <ThemeProvider theme={theme}>
+        <Router />
+      </ThemeProvider>
     </div>
   );
 }
